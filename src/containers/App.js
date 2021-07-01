62b161fbb2a31454ec6ef2453f57a43303b62bd4
ciamac-da <ciamac.davoudi@gmail.com>
2021-07-01 13:28:27 +0200

    Verified by Marti4n v1.0.4 replacing async await

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,13 +6,10 @@ import './App.css';
 function App() {
 
 const [movies, setMovies] = useState([])
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films/")
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-    const transformedMovies = data.results.map(movieData => {
+   const fetchMoviesHandler = async () => {
+     const response = await fetch("https://swapi.dev/api/films/")
+     const data = await response.json()
+     const transformedMovies = data.results.map(movieData => {
       return {
         id: movieData.episode_id,
         title: movieData.title,
@@ -21,7 +18,6 @@ const [movies, setMovies] = useState([])
       } 
     });
     setMovies(transformedMovies)
-    })
     .catch()
   }
 

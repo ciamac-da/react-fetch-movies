6572d01872ed33c35fc1e7681c14293231eb853f
ciamac-da <ciamac.davoudi@gmail.com>
2021-07-01 14:13:00 +0200

    Verified by Marti4n v1.0.5 error handling

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,41 @@
 import React, {useState} from 'react';
-
 import MoviesList from '../components/MoviesList';
 import './App.css';
 
 function App() {
 
 const [movies, setMovies] = useState([])
-   const fetchMoviesHandler = async () => {
-     const response = await fetch("https://swapi.dev/api/films/")
-     const data = await response.json()
-     const transformedMovies = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.release_date,
-        openingText: movieData.opening_crawl
-      } 
-    });
-    setMovies(transformedMovies)
-    .catch()
+const [isLoading, setIsLoading] = useState(false)
+const [error, setError] = useState(null)
+   
+
+
+const fetchMoviesHandler = async () => {
+     setIsLoading(true)
+     setError(null)
+     try{
+       const response = await fetch("https://swapi.dev/api/films/")
+       
+       if(!response.ok){
+         throw new Error("Something went wrong!")
+        }
+        
+        const data = await response.json()
+      
+        const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          openingText: movieData.opening_crawl
+        } 
+      });
+      setMovies(transformedMovies)
+      setIsLoading(false)
+    } catch(error) {
+      setError(error.message)
+      setIsLoading(false)
+    }
   }
 
   return (
@@ -27,10 +44,12 @@ const [movies, setMovies] = useState([])
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+       { !isLoading && movies.length > 0 && <MoviesList movies={movies} />} 
+       { !isLoading && movies.length === 0 && !error && <p>Found no movies...!</p> } 
+       { isLoading  && <p>Loading...</p> } 
+       { !isLoading && error && <p>{error}</p> } 
       </section>
     </React.Fragment>
   );
 }
-
 export default App;

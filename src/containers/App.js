bec4a7bd64e1c7e5a838e26590f823e25152f9e2
ciamac-da <ciamac.davoudi@gmail.com>
2021-07-01 12:12:09 +0200

    Verified by Marti4n v1.0.2

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React from 'react';
+
+import MoviesList from '../components/MoviesList';
+import './App.css';
 
 function App() {
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+
   return (
-    <div>
-     <h1>Hi</h1>
-    </div>
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
 }
 

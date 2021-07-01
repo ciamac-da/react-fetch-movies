c51ef6cb5174306e13176375205c8f9848d0e4bc
ciamac-da <ciamac.davoudi@gmail.com>
2021-07-01 15:21:52 +0200

    Verified by Marti4n v1.0.7 starwarsAPI done

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import MoviesList from '../components/MoviesList';
+import AddMovie from '../components/AddMovie';
+
 import './App.css';
 
 function App() {
@@ -41,18 +43,31 @@ const fetchMoviesHandler =  useCallback(async()=>{
     fetchMoviesHandler()
   }, [fetchMoviesHandler])
 
-  
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-       { !isLoading && movies.length > 0 && <MoviesList movies={movies} />} 
-       { !isLoading && movies.length === 0 && !error && <p>Found no movies...!</p> } 
-       { isLoading  && <p>Loading...</p> } 
-       { !isLoading && error && <p>{error}</p> } 
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
